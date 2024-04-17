b24a2bc728bf8c5953e61e46c0e69014acc277f2
dcastrom123 <diego.castro.maira+gmail.com@gmail.com>
2024-04-16 20:18:20 -0400

    DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 
 import { AppComponent } from './app.component';
-//import { CounterComponnet } from './counter/components/counter/counter.component';
-//import { HeroComponent } from './heroes/hero/hero.component';
-//import { ListComponent } from './heroes/list/list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-  //  CounterComponnet,
-    //HeroComponent,
-    //ListComponent
   ],
   imports: [
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
     AppRoutingModule
   ],
   providers: [],
